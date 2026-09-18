f56e5f3218ff4035e835631facb0ae57b37c3727
anjivo <shailesh5670000@gmail.com>
2026-09-18 17:46:14 +0530

    Create categories.ts

---
+++ data/categories.ts
@@ -0,0 +1,26 @@
+export const categories = [
+  {
+    name: "Fashion",
+    icon: "👗"
+  },
+  {
+    name: "Beauty",
+    icon: "💄"
+  },
+  {
+    name: "Toys",
+    icon: "🧸"
+  },
+  {
+    name: "Jewellery",
+    icon: "💍"
+  },
+  {
+    name: "Footwear",
+    icon: "👟"
+  },
+  {
+    name: "Home & Living",
+    icon: "🏠"
+  }
+];
